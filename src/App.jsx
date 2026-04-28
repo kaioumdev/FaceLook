19f13b24ed3b79c29f41ed47fe15fb526e5b72cf
kaioumdev <afrankaiyum34@gmail.com>
2026-04-28 22:22:29 +0600

    configure react-router-dom & implements successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
+import RegistrationPage from "./pages/RegistrationPage";
+import NotFountPage from "./pages/NotFountPage";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-red-500">This is just demo</h1>
+    <Routes>
+      <Route element={<HomePage></HomePage>} path="/"  /> 
+      <Route element={<LoginPage></LoginPage>} path="/login" />
+      <Route element={<ProfilePage></ProfilePage>} path="/me" />
+      <Route element={<RegistrationPage></RegistrationPage>} path="/register" ></Route>
+      <Route element={<NotFountPage></NotFountPage>} path="*" />
+    </Routes>
     </>
   );
 }
