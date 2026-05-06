7ddf30d24f3c5043513424aa11a3e0c245524226
kaioumdev <afrankaiyum34@gmail.com>
2026-05-06 12:11:18 +0600

    make a profileProvider

---
+++ src/provider/ProfileProvider.jsx
@@ -0,0 +1,15 @@
+import { useReducer } from "react"
+import { initialState, profileReducer } from "../reducers/profileReducer"
+import { ProfileContext } from "../context";
+
+const ProfileProvider = ({children}) => {
+    const [state, dispatch] = useReducer(profileReducer, initialState);
+
+    return (
+        <ProfileContext.Provider>
+            {children}
+        </ProfileContext.Provider>
+    )
+};
+
+export default ProfileProvider;
