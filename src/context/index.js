34781412f57daf7a2ad9e1a7d5a23130d03e0642
kaioumdev <afrankaiyum34@gmail.com>
2026-05-07 20:08:00 +0600

    create usePost hook components successfully

--- src/context/index.js
+++ src/context/index.js
@@ -2,5 +2,7 @@ import { createContext } from "react";
 
 const AuthContex = createContext();
 const ProfileContext = createContext();
+const PostContext = createContext();
 
-export {AuthContex, ProfileContext};
+
+export {AuthContex, ProfileContext, PostContext};
